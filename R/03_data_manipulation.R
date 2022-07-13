815feaa03b325ba040f32378703a4f88f9879d06
amandsol <oliveira.amands96@gmail.com>
2022-07-13 13:00:13 -0300

    adding script for eda after class

--- R/03_data_manipulation.R
+++ R/03_data_manipulation.R
@@ -177,6 +177,13 @@ write.csv(x = comm_total,
           row.names = FALSE)
 
 # Extra: filter ----------------------------------------------------------------
+#Elev is a variable
 sp1 <- filter(comm_total, TaxCode == "sp1", Elev > 3)
-
 View(sp1)
+
+sp1_2 <- comm_total[comm_total$TaxCode == "sp1" & comm_total$Elev >3,]
+
+select_test <- select(comm_total, TaxCode, Abundance, Sites)
+
+select_test2 <- comm_total[,c("TaxCode","Abundance","Sites")]
+
