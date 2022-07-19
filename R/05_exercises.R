0a1fd76d6227d573a5b96e5a7b1a9c39494d1b6d
amandsol <oliveira.amands96@gmail.com>
2022-07-19 11:28:56 -0300

    adding the exercise script

---
+++ R/05_exercises.R
@@ -0,0 +1,23 @@
+data("iris")
+iris
+
+
+
+oi<- filter(iris, Species == "setosa" | Species == "virginica")
+
+
+taxon <- data.frame(Family = "Iridaceae", Genus = "Iris",
+                      Species = c("setosa", "versicolor", "virginica"))
+
+t_taxon <- full_join(taxon,iris, by= "Species")
+
+ggplot(iris) +
+geom_boxplot(aes(y= Petal.Length))
+
+ggplot(iris) +
+  geom_boxplot(aes(y=Petal.Length, fill = Species))
+
+ggplot(iris)+
+  geom_point(mapping = aes(x=Sepal.Length, y= Sepal.Width))+
+  facet_wrap(~Species)
+
